77a837a5a0fc035b279e146520ebc5c20fcce1ac
Anton <kanes@inbox.ru>
2018-11-16 22:37:49 +0300

    Правки

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var fireballSize = 22;
 var wizardSpeed = 3;
 var wizardWidth = 70;
 
 // Скорость файерболов в зависимости от направления
-var getFireballSpeed = function(left) {
+var getFireballSpeed = function (left) {
   if (left) {
     return 5;
   }
@@ -11,16 +13,16 @@ var getFireballSpeed = function(left) {
 };
 
 // Пропорциональная высота мага
-var getWizardHeight = function() {
+var getWizardHeight = function () {
   return 1.337 * wizardWidth;
-}
+};
 
 // Расположение мага по горизонтали
-var getWizardX = function(width) {
+var getWizardX = function (width) {
   return (width - wizardWidth) / 2;
-}
+};
 
 // Расположение мага по вертикали
-var getWizardY = function(height) {
+var getWizardY = function (height) {
   return (height - getWizardHeight()) * 2 / 3;
-}
+};
