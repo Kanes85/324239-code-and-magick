a43fb59562445cfda6b7c6bf9263cc331b97e152
Kanes85 <kanes@inbox.ru>
2018-11-23 15:23:30 +0300

    Цвета столбцов

--- js/stat.js
+++ js/stat.js
@@ -12,8 +12,6 @@ var BAR_HEIGHT = CLOUD_HEIGHT - TEXT_HEIGHT - (GAP * 2); //высота стол
 var GAP_BAR = 50;
 var WIDTH_SCORE = 30;
 
-// var NAMES = ['ВЫ', 'Кекс', 'Катя', 'Игорь']; //Массив имен игроков
-
 // Отрисовка таблички
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -42,16 +40,23 @@ var renderStatistics = function (ctx, players, times) {
   ctx.fillText('Ура вы победили!', CLOUD_WIDTH / 2, FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_WIDTH / 2, FONT_GAP * 2);
 
-  // var players = ['Вы', 'Кекс', 'Катя', 'Алеся'];
-
   var maxTime = getMaxElement(times);
+
+  var players = [
+    {name: 'Вы', color: 'rgb(255, 0, 0, 1)'},
+    {name: 'Кекс', color: 'rgba(0, 72, 255, 0.9)'},
+    {name: 'Катя', color: 'rgba(55, 102, 221, 0.5)'},
+    {name: 'Алеся', color: 'rgba(25, 61, 154, 0.7)'}
+  ];
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000000';
 
     ctx.fillText(Math.floor(times[i]), BAR_HEIGHT + (BAR_WIDTH + GAP_BAR) * i,  BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime + TEXT_HEIGHT - WIDTH_SCORE);
 
-    ctx.fillText(players[i], BAR_HEIGHT + (BAR_WIDTH + GAP_BAR) * i, TEXT_HEIGHT + BAR_HEIGHT);
+    ctx.fillText(players[i].name, BAR_HEIGHT + (BAR_WIDTH + GAP_BAR) * i, TEXT_HEIGHT + BAR_HEIGHT);
+
+    ctx.fillStyle = players[i].color;
 
     ctx.fillRect(BAR_HEIGHT + (BAR_WIDTH + GAP_BAR) * i,  BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime + TEXT_HEIGHT - GAP, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
   }
